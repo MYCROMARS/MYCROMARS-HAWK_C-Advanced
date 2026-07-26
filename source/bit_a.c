3a80cff7eb60d7535808d09211ffc9ac4c9f0a02
Alexander Rudi <149962078+MYCROMARS-OPERATIONS@users.noreply.github.com>
2026-07-26 09:58:54 +0200

    dezimal to binär

--- source/bit_a.c
+++ source/bit_a.c
@@ -14,6 +14,15 @@ char s[50] = "";
 const char zero[2] = "0";
 const char one[2] = "1";
 
+// Create 
+char buffer[50];
+
+// Catch invalid input
+void scanString_bit(void){
+    // Input
+    scanf("%s", &buffer[0]);
+}
+
 // Main function
 void bit_a(void)
 {
@@ -21,43 +30,59 @@ void bit_a(void)
     printf("### BIT OPERATION A ###\n\n");
 
     // Create
-    int value = 2;
+    int val;
+    int valMax = 255;
+
     int count_zero;
     int count_one;
-    #define ar 7
+    #define ar 8  // 8 bit
 
-    int a[ar] = {0,0,0,0,0,0,0};
+    int val_bit;
+    
+    int a[ar] = {0,0,0,0,0,0,0,0};
+    int count_ar = 7;
+    
+    // Go here
+    main:
+    
+    // Output
+    printf("Input a Dezimal number 0...255 (8 bit): ");
 
-    switch (value)
-    {
-        case 0:
-            break;
-
-        case 1:
-            // 1
-            a[6] = 1;
-            break;
-
-        case 2:
-            // 2
-            a[5] = 1;
-            break;
-
-        case 3:
-            // 3
-            a[5] = 1;
-            a[6] = 1;
-            break;
-
-        case 4:
-            // Zahl 4
-            a[4] = 1;
-            break;
+    // Conditional statement
+    if (scanf("%d", &val) == 0)  {
+        // Output
+        printf("\nInput is not a number!\n\n");
         
-        default:
-            break;
+        // function: Catch invalid input
+        scanString_bit();
+        
+        // Jump to start
+        goto main;
     }
+    else if (val >= 0 && val <= valMax){
+        printf("\n");
 
+        int val_e = val;
+
+        // write binär numbers to array
+        while (val_e > 0)
+        {
+            val_bit = val_e % 2;            // R 0
+            val_e = val_e/2;                // 11
+            printf("value : 2 = %d R %d\n", val_e, val_bit);
+    
+            a[count_ar] = val_bit;
+            count_ar--;
+        }
+    }
+    else {
+        // Wrong number, not 1...menuMax
+        // Output
+        printf("\nInput is wrong, not 1...%d\n\n", valMax);
+
+        // Jump to start
+        goto main;
+    }
 
     // write binär number to string
     for (int i = 0; i < ar; i++)
@@ -78,24 +103,9 @@ void bit_a(void)
     // String to int
     long val2 = strtol(s, &endptr2, 2);
     
-    printf("\nvalue: %d\n", value);
-    printf("char string: %s\n", s);
+    //printf("\nInput: %d\n", val);
+    printf("\nBinär: %s\n", s);
     printf("Dezimal: %ld\n", val2);
-
-    
-    // Basis 2 für Binärzahlen angeben
-    //long val = strtol(bin_str, &endptr, 2);
-    
-    //printf("int: %d\n", x);
-
-    //printf("char *: %s\n", bin_str);
-
-    //printf("Dezimal: %ld\n", val);
-
-    // Sicherer, fals der Puffer zu klein ist.
-    // char text[20] = "Start";
-    // strncat(text, " und Ende", sizeof(text) - strlen(text) - 1);
-    // printf("%s\n", text);
 }
 
 // Count Zero
@@ -106,9 +116,3 @@ void _zero(){
 void _one(){
         strcat(s, one);
 }
-// void _one(int c_one){
-//     for (int i = 0; i < c_one; i++)
-//     {
-//         strcat(s, one);
-//     } 
-// }
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,8 +1,1 @@
-// Create 
-char buffer[50];
-
-// Catch invalid input
-void scanString0(void){
-    // Input
-    scanf("%s", &buffer[0]);
-}
+printf("Your input: ");
