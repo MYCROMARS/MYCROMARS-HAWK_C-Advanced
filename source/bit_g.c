f90d62d43ec2a49289916d82d1dc21ddac125bdc
Alexander Rudi <149962078+MYCROMARS-OPERATIONS@users.noreply.github.com>
2026-07-31 10:09:53 +0200

    Bit G

--- source/bit_g.c
+++ source/bit_g.c
@@ -5,59 +5,49 @@
 
 // Bit F
 
-// Function definition
-void input_zero_g1();
-void input_one_g1();
-
-void input_minus_g();
-
-int function_negative(int value);
-int binary_to_array(int count, int value, int array[]);
-
-// Create
+// Create global variables --------------------
 char bit_string_g1[50] = "";
-
 char buffer_bit_g[50];
 
-// Create & assign
 const char zero_g1[2] = "0";
 const char one_g1[2] = "1";
 const char minus_g[2] = "-";
 
-// Catch invalid input
-void scanString_bit_g(void){
-    // Input
-    scanf("%s", &buffer_bit_g[0]);
-    buffer_bit_g[0] = '\0';
-}
+#define BITS_G 16   // 16 bit
 
-// Main function
+// Function definition ------------------------
+void scanString_bit_g(void);
+void input_zero_g1(void);
+void input_one_g1(void);
+void input_minus_g(void);
+
+int function_negative(int value);
+int binary_to_array(int count, int value, int array[]);
+int array_to_string(int negative, int array[]);
+void decimal_to_binary(int bit_size, int value, int bit);
+
+
+// Main function ------------------------------
 void bit_g(void)
 {
     // Output
     printf("### BIT OPERATION G ###\n");
     printf("Decimal to Binary Loop\n\n");
 
-    // Create & assign
+    // Create & assign local variables
+    int menu;
     int value_g1;
     int value_g1_1;
-  
     int bit_g1;     // placeholder for bit
     int bit_g2;     // placeholder for bit
-   
     int bit_size_g1;
+    long binary_to_int_g1;
     char *hold_bits;
     int max_g = 255;
-
-    long binary_to_int_g1;
     int negative_g = 0;
-
-    int menu;
-
-    #define BITS_G 16   // 16 bit
     int count_g = BITS_G-1;
-
-    int array_bits_g1[BITS_G] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};  // String 1 Placeholder for bit's
+    // Placeholder for bits
+    int array_bits_g1[BITS_G] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};  
     
     // Go here
     main_1:
@@ -83,6 +73,7 @@ void bit_g(void)
         // Jump to start
         goto main_1;
     }
+    // Start input a number
     else if (menu == 1){
 
         // Output
@@ -121,64 +112,37 @@ void bit_g(void)
                 value_g1_1 = function_negative(value_g1_1);
             }
         
-            // Function: write Binary numbers to array
+            // Function: write binary numbers to array
             array_bits_g1[0] = binary_to_array(count_g, value_g1_1, array_bits_g1);
          
-    
-            // Loop: write Array number to string 
-            for (int i = 0; i < BITS_G; i++)
-            {
-                if (negative_g == 1){
-                    // Function input minus
-                    input_minus_g();
-                    negative_g = 2;
-                }
-                if (array_bits_g1[i] == 0){
-                    // Function input zero
-                    input_zero_g1();
-                }
-                else if (array_bits_g1[i] == 1){
-                    // Function input one
-                    input_one_g1();
-                }
-            }
-             
+            // Function: write array number to string 
+            negative_g = array_to_string(negative_g, array_bits_g1);
+
             // Output
-            if ( negative_g == 2)
-            {
+            if ( negative_g == 2){
                 printf("\nBinary String: %s\n", bit_string_g1);
             }
             else{
                 printf("\nBinary String:  %s\n", bit_string_g1);
-                
             }
-    
-    
-            // ----------------------------------------------------
+            
+            // Decimal to Binary with: bitwise shift right
+            decimal_to_binary(bit_size_g1, value_g1, bit_g2);
+            
+            // Assign: reset
+            value_g1 = 0;
+            value_g1_1 = 0;
+            bit_g1 = 0;     
+            bit_g2 = 0;    
+            bit_size_g1 = 0;
+            negative_g = 0;
+
+            for (int i = 0; i<BITS_G; i++){
+                array_bits_g1[i] = 0; 
+            }
+
             // Delate string
             bit_string_g1[0] = '\0';
-            
-            // Assign
-            bit_size_g1 = sizeof(value_g1) * 4; // 4*4=16
-            
-            // Output
-            printf("Binary:\t\t");
-    
-            // Loop: Value 1 in Binary
-            for (int i = bit_size_g1-1; i >= 0; i--) {
-    
-                // Assign: odd number, bit= 1 
-                bit_g2 = (value_g1 >> i) & 1;   
-    
-                // Output
-                printf("%d", bit_g2);
-                //if (i % 8 == 0) printf(" "); // spacing after 8 bits
-            }
-    
-            printf("\n\n");
-    
-            // Assign
-            negative_g = 0;
 
             // Jump to start
             goto main_1;
@@ -186,8 +150,61 @@ void bit_g(void)
     }
     else if (menu == 0){
         printf("\n### EXIT ###\n");
-    }
+    } 
+}
+
+// ---------------------------------------
+// Catch invalid input 
+void scanString_bit_g(void){
+    // Input
+    scanf("%s", &buffer_bit_g[0]);
+    buffer_bit_g[0] = '\0';
+}
+
+// Function: switch negative to positive 
+int function_negative(int value){
+    value = value * -1;
     
+    return value;
+}
+
+// Function: write binary numbers to array
+int binary_to_array(int count, int value, int array[]){
+    int bit;
+    
+    while (value > 0)
+    {
+        bit = value % 2; 
+        value = value/2;  
+        array[count] = bit;
+        count--;
+    }
+
+    return array[0];
+}
+
+// Function: write array number to string 
+int array_to_string(int negative, int array[]){
+
+    for (int i = 0; i < BITS_G; i++)
+    {
+        // Function input minus
+        if (negative == 1){
+            input_minus_g();
+            negative = 2;
+        }
+
+        // Function input zero
+        if (array[i] == 0){
+            input_zero_g1();
+        }
+        // Function input one
+        else if (array[i] == 1){
+            input_one_g1();
+        }
+    }
+
+    return negative;
 }
 
 // Function: Input zero ----------------
@@ -200,32 +217,33 @@ void input_one_g1(){
         strcat(bit_string_g1, one_g1);
 }
 
-// Function: Input minus ----------------
+// Function: Input minus 
 void input_minus_g(){
         strcat(bit_string_g1, minus_g);
 }
 
-// Function: switch negative to positive
-int function_negative(int value){
-    value = value * -1;
+
+// Decimal to Binary with: bitwise shift right
+void decimal_to_binary(int bit_size, int value, int bit){
+
+    // Output
+    printf("Binary:\t\t");
     
-    return value;
+    // Assign
+    bit_size = sizeof(value) * 4; // 4*4=16
+    
+    // Loop: Value 1 in Binary with: bitwise shift right
+    for (int i = bit_size-1; i >= 0; i--) {
+    
+        // Assign: odd number, bit= 1 
+        bit = (value >> i) & 1;   
+    
+        // Output
+        printf("%d", bit);
+        //if (i % 8 == 0) printf(" "); // spacing after 8 bits
+    }
+
+    printf("\n\n");
 }
 
-   int binary_to_array(int count, int value, int array[]){
-        int bit;
-        
-        while (value > 0)
-        {
-            
-            bit = value % 2; 
 
-            value = value/2;  
-
-            array[count] = bit;
-
-            count--;
-        }
-
-        return array[0];
-    }
